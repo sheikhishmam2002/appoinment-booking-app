ceaccb62f3e9eee5e54d034bba61c9defb95a4fe
sheikhishmam2002 <latifurahman2@gmail.com>
2025-02-14 22:35:22 +0600

    initial commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20">
+      {/* Left Side */}
+
+      <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]">
+        <p>
+          Book Appointment <br /> With Trusted Doctors
+        </p>
+        <div>
+          <img src={assets.group_profiles} alt="" />
+          <p>
+            Simply browse though our extensive list of trusted doctors,
+            <br /> schedule your appointment hassle-free.
+          </p>
+        </div>
+        <a href="">
+          Book appointment <img src={assets.arrow_icon} alt="" />
+        </a>
+      </div>
+
+      {/* Right Side */}
+
+      <div>
+        <img src={assets.header_img} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
+
+// 57:32
